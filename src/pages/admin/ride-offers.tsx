c19c9536615149dfd244779a298250fcf602a7c7
FevenBelay23 <fevenbelay942@gmail.com>
2023-06-28 18:04:35 +0300

    implement pagination and search bar with filter

--- src/pages/admin/ride-offers.tsx
+++ src/pages/admin/ride-offers.tsx
@@ -1,27 +1,21 @@
 import withAdminLayout from "@/components/common/admin/withAdminLayout";
 import Head from "next/head";
-import React from "react";
-import Final from "@/components/admin/ride-offers/RideOffersList";
-import FilterOffers from "@/components/admin/ride-offers/FilterOffers";
+import React, { useState } from "react";
+import RideOffersList from "@/components/admin/ride-offers/RideOffersList";
 
 type RideOffersProps = {};
 
 const RideOffers = (props: RideOffersProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const [status, setStatus] = useState("all");
+
   return (
     <div>
       <Head>
         <title>Ride Offers</title>
       </Head>
-      <FilterOffers
-        options={[]}
-        onSelect={function (value: string): void {
-          throw new Error("Function not implemented.");
-        }}
-        onSearch={function (query: string): void {
-          throw new Error("Function not implemented.");
-        }}
-      />
-      <Final />
+      <RideOffersList />
     </div>
   );
 };
