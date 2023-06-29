33b81d1f62d30a5d529ff6891c1e03e5dfeb4228
kidus <kidusabebe101@gmail.com>
2023-06-29 22:57:54 +0300

    Resolving conflict in index.tsxx 2

--- src/components/admin/users/drivers/Vehicles.tsx
+++ src/components/admin/users/drivers/Vehicles.tsx
@@ -1,65 +1,66 @@
 import React from 'react';
 import { Document, Page } from 'react-pdf';
 import { pdfjs } from 'react-pdf';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import Link from 'next/link'; // Import the Link component
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
 import { useGetVehicleByIdQuery } from '@/store/api';
 
-
 const Vehicles = () => {
-    const pageNumber = 1
+  const pageNumber = 1;
+  const router = useRouter();
+  const vehicleId = router.query.userId as string;
 
-    const router = useRouter();
-    const id = router.query.id
+  const { data, isLoading, isError } = useGetVehicleByIdQuery(vehicleId);
 
-    const {  data , isLoading , isError } = useGetVehicleByIdQuery(1)
+  console.log(data);
 
-    console.log(data)
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
 
-    if (isLoading){
-        return <div> Loading ...</div>
-    }
-
-    if (isError){
-        return <div> Error </div>
-    }
+  if (isError) {
+    return <div>Error</div>;
+  }
 
   return (
+    <div className="bg-white mx-10 lg:px-20 lg:mx-40 my-5 py-8 border border-gray-300 rounded-3xl">
+      <div className="grid grid-cols-2 gap-4 min-w-[500px]">
+        {/* First column */}
+        <div className="w-1/4 mx-2 bg-white">
+          <div className="bg-white p-4">
+            <span>License Number</span>
+          </div>
+          <div className="bg-white p-4">
+            <span>Experience</span>
+          </div>
+          <div className="bg-white p-4">
+            <span>Address</span>
+          </div>
+          <div className="bg-white p-4">
+            <span>Rate Number</span>
+          </div>
+        </div>
 
-    <div className="flex justify-center max-w-lg border rounded-lg shadow-md mx-auto p-8 h-screen">
-      {/* Left column */}
-      <div className="w-1/2 mx-2 bg-white">
-        <div className="bg-white p-4">
-          <p>Model</p>
-        </div>
-        <div className="bg-white p-4">
-          <p>Phone Number</p>
-        </div>
-        <div className="bg-white p-4">
-          <p>Number of Seats</p>
-        </div>
-        <div className="bg-white p-4">
-          <p>Libre</p>
-        </div>
-      </div>
-
-      {/* Right column */}
-      <div className="w-1/2 mx-2 bg-white ">
-        <div className="bg-white p-4">
-          <p className="bg-gray-100 px-6 flex justify-center">{data?.model}</p>
-        </div>
-        <div className="bg-white p-4">
-          <p className="bg-gray-100 px-6 flex justify-center">{data?.plateNumber}</p>
-        </div>
-        <div className="bg-white p-4">
-          <p className="bg-gray-100 px-6 flex justify-center">{data?.numberOfSeats}</p>
-        </div>
-        <div className="bg-white p-4">
+        {/* Second column */}
+        <div className="w-3/4 mx-2 bg-white">
+          <div className="bg-white p-4">
+            <span>{data?.model}</span>
+          </div>
+          <div className="bg-white p-4">
+            <span>{data?.plateNumber}</span>
+          </div>
+          <div className="bg-white p-4">
+            <span>{data?.numberOfSeats}</span>
+          </div>
+          <div className="bg-white p-4">
           <a href={data?.libre} target="_blank" rel="noopener noreferrer">
-            <Document className="custom-document"  file={data?.libre}>
-              <Page pageNumber={pageNumber} width={200} height={300}   />
-            </Document>
-          </a>
+              <button className="bg-blue-500 text-white mt-6 px-4 py-2 rounded">
+                Open 
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </div>
